2d80a4f26e8e6242dec8d64be64f5cd53f7c628a
Mr <Mr@Mr-PC>
2018-04-26 22:28:07 +0530

    hello

--- src/main/java/com/qspiders/pack/App.java
+++ src/main/java/com/qspiders/pack/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("I love github and Maven");
+        System.out.println("I Love github and Maven");
     }
 }
