8a719f0a4f40860af8c5b4839c9e4fe611f06d60
Mr <Mr@Mr-PC>
2018-04-28 13:45:04 +0530

    commit1

--- src/main/java/com/qspiders/pack/App.java
+++ src/main/java/com/qspiders/pack/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("I Love Github and Maven");
+        System.out.println("I Love GitHub and Maven");
     }
 }
